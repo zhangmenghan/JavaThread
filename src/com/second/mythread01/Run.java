4b4ee1f95edb6ebba560344d3813792b149afdd9
zhangmenghan <2423306508@qq.com>
2019-08-06 11:28:55 +0800

    1

---
+++ src/com/second/mythread01/Run.java
@@ -0,0 +1,18 @@
+package com.second.mythread01;
+
+public class Run {
+
+	public static void main(String[] args) {
+
+		HasSelfPrivateNum numRef1 = new HasSelfPrivateNum();
+		HasSelfPrivateNum numRef2 = new HasSelfPrivateNum();
+
+		ThreadA athread = new ThreadA(numRef1);
+		athread.start();
+
+		ThreadB bthread = new ThreadB(numRef2);
+		bthread.start();
+
+	}
+
+}
---
+++ src/com/second/mythread04/ThreadB.java
@@ -0,0 +1,18 @@
+package com.second.mythread04;
+
+public class ThreadB extends Thread {
+
+	private HasSelfPrivateNum numRef;
+
+	public ThreadB(HasSelfPrivateNum numRef) {
+		super();
+		this.numRef = numRef;
+	}
+
+	@Override
+	public void run() {
+		super.run();
+		numRef.addI("b");
+	}
+
+}
